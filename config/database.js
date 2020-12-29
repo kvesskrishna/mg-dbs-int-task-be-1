35d432f93b6cc37dc5e1f14e3dfeb2eef2f17ec7
MSS User <mss.team.project@gmail.com>
2020-12-29 14:02:32 +0530

    config updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
     database: 'mongodb+srv://betcric:Hl73JzIa7y5Rvpdt@cluster0-vzmrv.mongodb.net/test?retryWrites=true&w=majority',
-    // mongodb+srv://betcric:Hl73JzIa7y5Rvpdt@cluster0-vzmrv.mongodb.net/test?retryWrites=true&w=majority
-    // database: 'mongodb://134.209.126.55/l7exchange?authSource=admin&retryWrites=true&w=majority',
     secret: 'donotgivethemwings'
 }
